d1c89a1b6ee466c96059bad45c0b395c0055bc79
xur86 <xur86@mcmaster.ca>
2023-04-08 20:39:01 -0400

    separating models

--- src/draw_curve.py
+++ src/draw_curve.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 
 full_fps = np.array([16.7, 23.1, 28.3, 30.3, 31.5])               # FPS
-full_mota = np.array([68.5, 66.3, 61.1, 57.6, 53.8])             # MOTA
+full_mota = np.array([71.8, 69.6, 65.5, 63.3, 59.3])             # MOTA
 
-half_fps = np.array([25.8, 31.6, 35.2, 34.2, 38.4])               # FPS
+half_fps = np.array([23.4, 29.5, 33.7, 35.3, 35.6])               # FPS
 half_mota = np.array([66.6, 63.9, 57.8, 55.0, 52.3])            # MOTA
 
-quarter_fps = np.array([29.1, 34.1, 37.3, 36.2, 37.5])               # FPS
+quarter_fps = np.array([27.8, 32.7, 35.3, 36.8, 36.7])               # FPS
 quarter_mota = np.array([61.5, 59.5, 57.1, 53.1, 48.3])            # MOTA
 
-DC_fps = np.array([34.0, 33.5, 31.3, 29.7, 27.9, 25.0, 19.7, 26.2, 18.5, 17.4, 17.1])               # FPS
-DC_mota = np.array([58.0, 59.1, 59.7, 61.8, 63.5, 65.9, 68.6, 64.7, 68.6, 68.9, 69.1])          # MOTA
+DC_fps = np.array([32.6, 32.3, 30.8, 29.5, 26.9, 24.8, 19.5, 26.4, 18.1, 17.4, 16.8])               # FPS
+DC_mota = np.array([59.1, 60.0, 61.8, 63.2, 65.1, 66.7, 70.2, 64.7, 70.9, 71.0, 71.2])          # MOTA
 
 # DC_fps_classifier = np.array([32.7, 30.8, 31.0, 28.7, 26.6, 23.8])               # FPS
 # DC_mota_classifier = np.array([58.3, 59.2, 60.0, 62.4, 63.9, 65.9])          # MOTA
@@ -32,9 +32,9 @@ for i, txt in enumerate(resolutions):
     plt.annotate(txt, (half_fps[i], half_mota[i]))
     plt.annotate(txt, (quarter_fps[i], quarter_mota[i]))
 
-thresh_settings = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11']
-for i, txt in enumerate(thresh_settings):
-    plt.annotate(txt, (DC_fps[i], DC_mota[i]))
+# thresh_settings = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11']
+# for i, txt in enumerate(thresh_settings):
+#     plt.annotate(txt, (DC_fps[i], DC_mota[i]))
 
 # Save the plot to a file
 plt.savefig('mota_vs_fps.png', dpi=300, bbox_inches='tight')
--- src/exps_script.py
+++ src/exps_script.py
@@ -7,14 +7,15 @@ import time
 
 # track_half_multiknob with different sp and thresh
 # sp_list = [40]
-# thresh_list = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6']
+# thresh_list = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11']
 # for sp in sp_list:
 #     for thresh in thresh_list:
-#         cmd_str = 'python track_half_multiknob.py --exp_id profiling_multiknob_{}_{} --task mot_multiknob \
-#                --load_model ../models/full-dla_34-multiknob.pth --load_half_model ../models/half-dla_34.pth --load_quarter_model ../models/quarter-dla_34.pth \
-#                --switch_period {} --threshold_config {}'.format(sp, thresh, sp, thresh)
+#         cmd_str = 'python track_half_multiknob.py --exp_id multiknob_{}_{} --task mot_multiknob \
+#                 --load_model ../exp/mot_multiknob/gen_datasets_multiknob_full_crowdhuman/full-dla_34-multiknob.pth \
+#                 --load_half_model ../exp/mot_multiknob/gen_datasets_multiknob_full_crowdhuman/half-dla_34.pth \
+#                 --load_quarter_model ../exp/mot_multiknob/gen_datasets_multiknob_full_crowdhuman/quarter-dla_34.pth \
+#                 --switch_period {} --threshold_config {}'.format(sp, thresh, sp, thresh)
 #         os.system(cmd_str)
-#         time.sleep(5)
 
 # track_half with different model and imgsz
 imgsz_list = [(1088, 608), (864, 480), (704, 384), (640, 352), (576, 320)]
