c58e38098057ba1f155d1bcd321a755941b3f3d8
xur86 <xur86@mcmaster.ca>
2023-04-09 15:22:08 -0400

    update fps profile

--- src/track_half_multiknob.py
+++ src/track_half_multiknob.py
@@ -51,9 +51,10 @@ def compare_hms(hm, hm_knob):
 #     return best_config
 
 def update_config(det_rate_list, threshold_config):                      # the threshold is step-wise               
-    config_fps_sorted = [11, 14, 13, 8, 10, 7, 12, 5, 9, 4, 6, 2, 3, 1, 0]      # the avg fps of the configurations from high to low
+    # config_fps_sorted = [11, 14, 13, 8, 10, 7, 12, 5, 9, 4, 6, 2, 3, 1, 0]      # the avg fps of the configurations from high to low: averaged by 3 runs
+    config_fps_sorted = [14, 11, 13, 8, 10, 7, 5, 12, 9, 4, 6, 2, 1, 3, 0]      # the avg fps of the configurations from high to low: averaged by 10 runs
     thresholds = []
-    thresholds_preset = [0.61, 0.63, 0.68, 0.62, 0.64, 0.70, 0.65, 0.67, 0.73, 0.66, 0.69, 0.74, 0.71, 0.72, 0.75]
+    thresholds_preset = [0.61, 0.66, 0.71, 0.62, 0.67, 0.72, 0.63, 0.68, 0.73, 0.64, 0.69, 0.74, 0.65, 0.70, 0.75]
     if threshold_config == 'C1':
         thresholds = thresholds_preset
     if threshold_config == 'C2':
